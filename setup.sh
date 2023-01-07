40ad4febcac02c735d30749b54badd10ec6036ef
Manuel Schaich <rmschaich@outlook.com>
2023-01-07 13:13:41 +0000

    We don't get the full container environment from within script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Make sure one argument is passed
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <key>"
+    exit 1
+fi
+
+KEY=$1
+
 echo "Decrypting license file..."
-gpg --quiet --batch --yes --decrypt --passphrase=$THE_SECRET --output $HOME/license.lic $HOME/license.lic.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=$KEY --output $HOME/license.lic $HOME/license.lic.gpg
