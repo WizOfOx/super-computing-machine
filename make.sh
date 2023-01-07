03c5d2d60e240500440e66584169caa45dc28428
Manuel Schaich <rmschaich@outlook.com>
2023-01-07 11:25:22 +0000

    Place mex in base level then

--- make.sh
+++ make.sh
@@ -2,9 +2,8 @@
 
 MATLABROOT=/opt/matlab/R2022b
 
-ARGS="-v -output ipopt -outdir  $home/Documents/MATLAB/+bin -R2018a -I/build/include/coin-or -I/build/include -I/build/include/coin-or/mumps /build/lib/libipopt.a /build/lib/libcoinhsl.a /build/lib/libcoinmumps.a /usr/lib/x86_64-linux-gnu/openblas-pthread/libopenblas.a -lm -lpthread -lgfortran main.cpp"
+ARGS="-v -output ipopt -outdir  $home/Documents/MATLAB -R2018a -I/build/include/coin-or -I/build/include -I/build/include/coin-or/mumps /build/lib/libipopt.a /build/lib/libcoinhsl.a /build/lib/libcoinmumps.a /usr/lib/x86_64-linux-gnu/openblas-pthread/libopenblas.a -lm -lpthread -lgfortran main.cpp"
 
-mkdir -p $home/Documents/MATLAB/+bin
 
 echo $MATLABROOT/bin/mex $ARGS
 $MATLABROOT/bin/mex $ARGS
