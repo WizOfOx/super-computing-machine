c0020e9651cb1f331ec031677f10a037d436fbd4
Manuel Schaich <rmschaich@outlook.com>
2023-01-07 09:19:06 +0000

    Remove unused files

--- eigenConversion.hpp
+++
@@ -1,5 +0,0 @@
-#include "MatlabDataArray.hpp"
-#include <Eigen/Dense>
-
-Eigen::MatrixXd convert(const matlab::data::Array& x);
-matlab::data::TypedArray<double> convert(matlab::data::ArrayFactory& factory, const Eigen::MatrixXd& x);
