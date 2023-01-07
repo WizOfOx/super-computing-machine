dd55917fab4f64154698d77ae7cc037cbf1554db
Manuel Schaich <rmschaich@outlook.com>
2023-01-07 11:41:15 +0000

    Get matlab version of container from binary

--- make.sh
+++ make.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-MATLABROOT=/opt/matlab/R2022b
+MATLABVERSION=$(stat /usr/local/bin/matlab | grep -Eo "R[0-9]{4}[ab]")
+
+MATLABROOT=/opt/matlab/$MATLABVERSION
 
 ARGS="-v -output ipopt -outdir $HOME/Documents/MATLAB/+bin -R2018a -I/build/include/coin-or -I/build/include -I/build/include/coin-or/mumps /build/lib/libipopt.a /build/lib/libcoinhsl.a /build/lib/libcoinmumps.a /usr/lib/x86_64-linux-gnu/openblas-pthread/libopenblas.a -lm -lpthread -lgfortran main.cpp"
 
