9696bb1e7f173e7be30c94895163490f74dfecd3
Manuel Schaich <rmschaich@outlook.com>
2023-01-07 11:56:22 +0000

    Some more variable escaping

--- make.sh
+++ make.sh
@@ -2,9 +2,11 @@
 
 MATLABVERSION=$(stat /usr/local/bin/matlab | grep -Eo "R[0-9]{4}[ab]")
 
-MATLABROOT=/opt/matlab/$MATLABVERSION
+echo "MATLAB version: $MATLABVERSION"
 
-ARGS=-v -output ipopt -outdir $HOME/Documents/MATLAB/+bin -R2018a -I/build/include/coin-or -I/build/include -I/build/include/coin-or/mumps /build/lib/libipopt.a /build/lib/libcoinhsl.a /build/lib/libcoinmumps.a /usr/lib/x86_64-linux-gnu/openblas-pthread/libopenblas.a -lm -lpthread -lgfortran main.cpp
+MATLABROOT="/opt/matlab/${MATLABVERSION}"
+
+ARGS="-v -output ipopt -outdir ${HOME}/Documents/MATLAB/+bin -R2018a -I/build/include/coin-or -I/build/include -I/build/include/coin-or/mumps /build/lib/libipopt.a /build/lib/libcoinhsl.a /build/lib/libcoinmumps.a /usr/lib/x86_64-linux-gnu/openblas-pthread/libopenblas.a -lm -lpthread -lgfortran main.cpp"
 
 mkdir -vp $HOME/Documents/MATLAB/+bin
 
