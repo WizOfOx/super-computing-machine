c55daf6ed9aff70105e32c5326e8fdb80e3eaef5
Manuel Schaich <rmschaich@outlook.com>
2023-01-07 11:49:19 +0000

    Some more escaping for folder creation

--- make.sh
+++ make.sh
@@ -4,9 +4,9 @@ MATLABVERSION=$(stat /usr/local/bin/matlab | grep -Eo "R[0-9]{4}[ab]")
 
 MATLABROOT=/opt/matlab/$MATLABVERSION
 
-ARGS="-v -output ipopt -outdir $HOME/Documents/MATLAB/+bin -R2018a -I/build/include/coin-or -I/build/include -I/build/include/coin-or/mumps /build/lib/libipopt.a /build/lib/libcoinhsl.a /build/lib/libcoinmumps.a /usr/lib/x86_64-linux-gnu/openblas-pthread/libopenblas.a -lm -lpthread -lgfortran main.cpp"
+ARGS=-v -output ipopt -outdir $HOME/Documents/MATLAB/+bin -R2018a -I/build/include/coin-or -I/build/include -I/build/include/coin-or/mumps /build/lib/libipopt.a /build/lib/libcoinhsl.a /build/lib/libcoinmumps.a /usr/lib/x86_64-linux-gnu/openblas-pthread/libopenblas.a -lm -lpthread -lgfortran main.cpp
 
-mkdir -p $HOME/Documents/MATLAB/+bin
+mkdir -vp $HOME/Documents/MATLAB/+bin
 
 echo $MATLABROOT/bin/mex $ARGS
 $MATLABROOT/bin/mex $ARGS
