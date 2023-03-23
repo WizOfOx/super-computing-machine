2a44abdde18d454cb56cdcc84b65eb43323312a2
Manuel Schaich <rmschaich@outlook.com>
2023-03-23 19:22:07 +0000

    Another try

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -2,3 +2,5 @@
 
 /home/matlab/setup.sh $THE_SECRET
 /bin/run.sh -batch "cd('/workspace');run('tester.m')"
+testresult=$(cat $HOME/testresults.xml)
+echo "testresult='$testresult'" >> "$GITHUB_OUTPUT"
