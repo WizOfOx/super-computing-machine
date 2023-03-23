462d0fa07383d36f80966b0ea2359b7a5033beea
Manuel Schaich <rmschaich@outlook.com>
2023-03-23 19:38:43 +0000

    Maybe as a mount point..

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -2,5 +2,7 @@
 
 /home/matlab/setup.sh $THE_SECRET
 /bin/run.sh -batch "cd('/workspace');run('tester.m')"
-testresult=$(cat $HOME/testresults.xml)
-echo "testresult='$testresult'" >> "$GITHUB_OUTPUT"
+cd $HOME
+testresult=$(cat testresults.xml)
+echo "testresult='$testresult'" >> /output
+# echo $testresult
